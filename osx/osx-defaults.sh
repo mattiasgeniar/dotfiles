2315ca6b96509343b48a2b4aa51a10a12ae60f51
Mattias Geniar <m@ttias.be>
2017-04-07 11:35:00 +0200

    Add Atom editor configs

--- osx/osx-defaults.sh
+++ osx/osx-defaults.sh
@@ -122,7 +122,7 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Automatically illuminate built-in MacBook keyboard in low light
